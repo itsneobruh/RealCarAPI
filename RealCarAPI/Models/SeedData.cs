5b2fa4cd3b561c14910bcb20ed667fd0d7c4adda
itsneobruh <nixgt2@gmail.com>
2018-11-22 17:36:09 +1300

    Corrup file error handle

---
+++ RealCarAPI/Models/SeedData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RealCarAPI.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RealCarAPIContext(
+                serviceProvider.GetRequiredService<DbContextOptions<RealCarAPIContext>>()))
+            {
+                // Look for any movies.
+                if (context.CarItem.Count() > 0)
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.CarItem.AddRange(
+                    new CarItem
+                    {
+                        Title = "Evo 3",
+                        Url = "https://thecarblob.blob.core.windows.net/images/Evo3.jpg",
+                        Tags = "Mazda",  
+                        Width = "70",
+                        Height = "43",
+                        Engine = "1600cc",
+                        Cylinders = 4
+                    }
+
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
